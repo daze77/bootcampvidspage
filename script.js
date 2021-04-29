84866a94488263d3abda3cf897896e202c08baa0
daze_77 <daze77@gmail.com>
2021-04-29 17:09:07 -0400

    cleaned up script

--- script.js
+++ script.js
@@ -2,57 +2,6 @@ var classList = document.querySelector('ul')
 
 
 
-
-// function getInfo(){
-// fetch('/classvidsinfo.json')
-// .then(function (response) {
-//   return response.json();
-// })
-// .then(function (data) {
-//     console.log(data)
-//   for (var i = 0; i < data.length; i++) {
-//     var listItem = document.createElement('li');
-//     listItem.classList="list-group-item"
-//     listItem.textContent = data[i].SessionNumber + "  " + data[i].SessionName;
-//     // console.log(listItem)
-//     // console.log(classList)
-//     classList.appendChild(listItem);
-  
-
-//   for (var x=0; x<data.Videos; x++){
-//       console.log(data.Videos)
-//     var subList = document.createElement('li')
-//     subList.textContent=data[i]
-//     console.log(subList)
-
-
-
-//   }}
-// }
-
-
-
-
-
-
-
-
-
-
-// );
-
-// }
-// getInfo()
-
-
-
-
-
-
-
-
-
-
 fetch('/classvidsinfo.json')
 .then(response => response.json())
 .then(data => {
